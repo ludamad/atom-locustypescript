0e2c4a48236a4a73c16abe09c6af64a8e236bc6a
ludamad <domuradical@gmail.com>
2016-10-07 02:46:35 -0400

    Add readme, and make install.sh link package through apm.

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-
-# Good practice -- exit completely on any bad exit code:
 set -e
 
 npm install
 cp -r ntypescript node_modules/
 cd node_modules/ntypescript
 if [ ! -d ConcreteTypeScript ] ; then
-    git clone --depth 1 ssh://git@bitbucket.org/ludamad/ConcreteTypeScript1.6 ConcreteTypeScript
+    git clone --depth 1 https://github.com/ludamad/LocusTypeScript ConcreteTypeScript
 else
     cd ConcreteTypeScript/
     git pull
@@ -15,3 +13,5 @@ else
 fi
 
 ./prepare.sh
+cd ../..
+apm link
